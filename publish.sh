861586bf1b087bd0da735928355d61124c3b1eed
Manuel Bovo <manuel.bovo@sysdig.com>
2021-05-27 19:26:54 +0200

    Change dir

--- publish.sh
+++ publish.sh
@@ -2,7 +2,9 @@
 set -eo pipefail
 
 echo "==> Upload markdown files ${DOC_DIR}"
-find $(pwd)/${DOC_DIR} -type f -name '*.md' -not -name 'README.md' | while read file; do 
+pushd ${DOC_DIR}
+find $(pwd) -type f -name '*.md' -not -name 'README.md' | while read file; do 
     echo "Upload ${file}"; 
     mark -p ${CONFLUENCE_PASSWORD} -u ${CONFLUENCE_USERNAME} -b ${BASE_URL} --debug -f "${file}" > /dev/null 
 done
+popd
--- verify.sh
+++ verify.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -l
 
 echo "==> Verify markdown files into ${DOC_DIR}"
-find $(pwd)/${DOC_DIR} -type f -name '*.md' -not -name 'README.md' | while read file; do 
+pushd ${DOC_DIR}
+find $(pwd) -type f -name '*.md' -not -name 'README.md' | while read file; do 
     echo "Verify ${file}"; 
     mark -p ${CONFLUENCE_PASSWORD} -u ${CONFLUENCE_USERNAME} -b ${BASE_URL} --debug --compile-only -f "${file}" > /dev/null 
 done
+popd
