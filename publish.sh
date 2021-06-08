251a832753aa8727a410d543145708f4d6f32e2c
Manuel Bovo <manuel.bovo@gmail.com>
2021-06-08 19:44:08 +0200

    Revert "Avoid manual changes"
    This reverts commit bc16eaf3ba20930cc35b1a90af66ab73023f402c.

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@ set -eo pipefail
 
 echo "==> Upload markdown files into ${DOC_DIR}"
 pushd ${DOC_DIR}
-grep -R -l 'Space:' *.md | xargs -n1 -I{} mark -k -p ${CONFLUENCE_PASSWORD} -u ${CONFLUENCE_USERNAME} -b ${BASE_URL} --debug -f {} > /dev/null 
+grep -R -l 'Space:' *.md | xargs -n1 -I{} mark -p ${CONFLUENCE_PASSWORD} -u ${CONFLUENCE_USERNAME} -b ${BASE_URL} --debug -f {} > /dev/null 
 popd
