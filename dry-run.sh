b576836fbbbc3902f394cde9ff908ff8c5bd7f37
Manuel Bovo <manuel.bovo@gmail.com>
2021-06-08 19:24:56 +0200

    testing dry-run

--- dry-run.sh
+++ dry-run.sh
@@ -4,6 +4,6 @@ echo "==> Verify markdown files into ${DOC_DIR}"
 pushd ${DOC_DIR}
 # find $(pwd) -type f -name '*.md' -not -name 'README.md' | while read file; do 
 #     echo "Verify ${file}"; 
-    mark -p ${CONFLUENCE_PASSWORD} -u ${CONFLUENCE_USERNAME} -b ${BASE_URL} --debug --dry-run -f '*.md'  
+    mark -p ${CONFLUENCE_PASSWORD} -u ${CONFLUENCE_USERNAME} -b ${BASE_URL} --debug --dry-run -f '*.md' > /dev/null 
 # done
 popd
